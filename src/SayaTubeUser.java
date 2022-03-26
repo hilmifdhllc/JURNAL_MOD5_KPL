53fad7f22d87a349a13abca2f6b378cd651f791c
Hilmi FC <hilmie123013@gmail.com>
2022-03-26 15:39:34 +0700

    penambahan design by contract pada semua kelas

--- src/SayaTubeUser.java
+++ src/SayaTubeUser.java
@@ -1,4 +1,4 @@
-/*
+/* /Hilmi Fadhillah Cahyadi 1302200124
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
